3f99cf875b4406241dd272e3034968a3922e88eb
kzn <kzn@iitp.ru>
2010-01-15 17:46:30 +0300

    Added size field;

--- src/Sample.java
+++ src/Sample.java
@@ -2,10 +2,12 @@ import java.util.List;
 
 public class Sample extends SparseVector {
 	public double target;
+	public int size;
 	
 	
 	Sample(double target, List<Integer> idxs, List<Double> vals){
 		super(idxs, vals);
 		this.target = target;
+		this.size = idxs.size();
 	}
 }
