1d332e655bd27f294c8b9749536506926692e663
ant <devnull@localhost>
2010-01-18 00:43:23 +0300

    Added WeightVectorOVR class

---
+++ src/WeightVectorOVR.java
@@ -0,0 +1,16 @@
+
+public class WeightVectorOVR extends WeightVectorProxy {
+	protected double positive;
+	
+	public double getPositive() {
+		return positive;
+	}
+	public void setPositive(double positive) {
+		this.positive = positive;
+	}
+	public WeightVectorOVR(WeightVector base, double positive){
+		super(base);
+		this.positive = positive;
+	}
+
+}
