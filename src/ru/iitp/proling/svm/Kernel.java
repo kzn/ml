a793d0e2902aa20570ac0813536ca438a261ed67
ant <devnull@localhost>
2010-01-18 11:16:27 +0300

    Move to right package

---
+++ src/ru/iitp/proling/svm/Kernel.java
@@ -0,0 +1,10 @@
+package ru.iitp.proling.svm;
+import SparseVector;
+
+/*
+ * Abstract class instead of Interface for efficiency reasons
+ */
+public abstract class Kernel {
+	public abstract double dot(SparseVector x, SparseVector y);
+	public abstract double dot(double[] dense, SparseVector x);
+}
