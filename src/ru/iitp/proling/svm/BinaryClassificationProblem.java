2494f8f760d6b92a8ed89e084383cfb44f73a80a
ant <devnull@localhost>
2010-01-20 11:40:46 +0300

    Added new hier

---
+++ src/ru/iitp/proling/svm/BinaryClassificationProblem.java
@@ -0,0 +1,11 @@
+package ru.iitp.proling.svm;
+
+public class BinaryClassificationProblem extends Problem {
+
+	@Override
+	public Classifier solve(Dataset dataset, Solver solver) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/ru/iitp/proling/svm/Classifier.java
@@ -0,0 +1,13 @@
+package ru.iitp.proling.svm;
+import java.util.*;
+
+public abstract class Classifier {
+	/// Return actual label index
+	abstract int classify(SparseVector v);
+
+	/// Return sorted of distribution of scores for each class 
+	abstract SortedMap<Double, Integer> scores();
+	
+	/// Return label distribution
+	abstract Alphabet alphabet();
+}
