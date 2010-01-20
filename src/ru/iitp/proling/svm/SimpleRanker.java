d59f4fc3d478ab58b2e72cfd585022b61a55bd98
kzn <kzn@iitp.ru>
2010-01-20 19:04:33 +0300

    Added simple ranker kode

---
+++ src/ru/iitp/proling/svm/SimpleRanker.java
@@ -0,0 +1,31 @@
+package ru.iitp.proling.svm;
+
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class SimpleRanker implements Ranker {
+	protected Kernel k;
+	protected double[] w;
+	
+	public SimpleRanker(double[] vec, Kernel k){
+		this.w = vec;
+		this.k = k;
+	}
+
+	@Override
+	public SortedMap<Double, SparseVector> rank(List<SparseVector> lst) {
+		SortedMap<Double, SparseVector> sm = new TreeMap<Double, SparseVector>();
+		for(SparseVector v : lst)
+			sm.put(score(v), v);
+		
+		
+		return sm;
+	}
+
+	@Override
+	public double score(SparseVector x) {
+		return k.dot(w, x);
+	}
+
+}
