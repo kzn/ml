11295496da77c9a6ab456ec8837f5539a861aa98
kzn <kzn@iitp.ru>
2010-01-20 17:11:01 +0300

    Added toString method

--- src/ru/iitp/proling/svm/SparseVector.java
+++ src/ru/iitp/proling/svm/SparseVector.java
@@ -23,4 +23,18 @@ public class SparseVector {
 	public int size(){
 		return indexes.length;
 	}
+	
+	public String toString(){
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i = 0; i != size(); i++){
+			if(i != 0)
+				sb.append(' ');
+			sb.append(indexes[i]);
+			sb.append(':');
+			sb.append(values[i]);
+		}
+		
+		return sb.toString();
+	}
 };
