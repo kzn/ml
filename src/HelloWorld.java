525aea4ac193445d85e494c94ede2e7540b01fcb
ant <devnull@localhost>
2010-01-21 11:29:09 +0300

    Changed test program

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -11,6 +11,10 @@ import ru.iitp.proling.svm.*;
 
 
 public class HelloWorld {
+	
+	public static void longtest(long k){
+		System.out.println(k);
+	}
 	public static void main(String[] args){
 		
 		if(args.length > 0)
@@ -32,8 +36,10 @@ public class HelloWorld {
 		
 		for(int i = 0; i != dset.size(); i++)
 			targets[i] = t[i] == 1? 1.0 : -1.0;
+		//PolyKernel(1, 1, dset.max_dim())
 		
-		WeightVector vw = new WeightVectorLinear(dset, targets, new PolyKernel(1, 1, dset.max_dim()));
+		Kernel k = new PolyKernel(1, 1, dset.max_dim());
+		WeightVector vw = new WeightVectorLinear(dset, targets, new HashKernel(k, k.dim(dset.max_dim())));
 		DCDSolver.solve(vw, 0.05, 0.05, 500, 0.1, 1000000);
 		
 
@@ -44,6 +50,11 @@ public class HelloWorld {
 		System.out.println("Dual objective:" + vw.objectiveDual());
 		System.out.println("Zero-one loss:" + vw.zero_one_loss());
 		System.out.println(dset.alphabet());
+		
+		int m = 300000;
+		int n = 1000000;
+		
+		longtest((long)m *n);
 		
 	}
 
