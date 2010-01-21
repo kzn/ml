15dab8190372f0e28c70f88db8aac55c5bbd1421
kzn <kzn@iitp.ru>
2010-01-21 18:28:04 +0300

    Added virtual dataset class

---
+++ src/ru/iitp/proling/svm/VirtualDataset.java
@@ -0,0 +1,81 @@
+package ru.iitp.proling.svm;
+
+import java.util.ArrayList;
+
+import ru.iitp.proling.common.Alphabet;
+import gnu.trove.TIntArrayList;
+
+public class VirtualDataset<T> implements Dataset {
+	protected Alphabet<T> alphabet;
+	protected ArrayList<SparseVector> samples;
+	protected TIntArrayList targets;
+	protected TIntArrayList qids;
+	protected int dim;
+	
+	public VirtualDataset(Alphabet<T> alphabet){
+		this.alphabet = alphabet;
+		samples = new ArrayList<SparseVector>();
+		targets = new TIntArrayList();
+		qids = new TIntArrayList();
+		dim = 0;
+	}
+	
+	public int add(SparseVector v, int target, int qid){
+		int idx = samples.size();
+		samples.add(v);
+		targets.add(target);
+		qids.add(qid);
+		return idx;
+	}
+	
+	public int add(SparseVector v, int target){
+		return add(v, target, 0);
+	}
+	
+	public int add(SparseVector v, T target){
+		return add(v, alphabet.get(target), 0);
+	}
+
+	@Override
+	public Alphabet<T> alphabet() {
+		return alphabet;
+	}
+
+	@Override
+	public int max_dim() {
+		return dim;
+	}
+
+	@Override
+	public int qid(int idx) {
+		return qids.get(idx);
+	}
+
+	@Override
+	public int[] qids() {
+		return qids.toNativeArray();
+	}
+
+	@Override
+	public int size() {
+		return samples.size();
+	}
+
+	@Override
+	public int target(int idx) {
+		// TODO Auto-generated method stub
+		return targets.get(idx);
+	}
+
+	@Override
+	public int[] targets() {
+		return targets.toNativeArray();
+	}
+
+	@Override
+	public SparseVector vec(int idx) {
+		// TODO Auto-generated method stub
+		return samples.get(idx);
+	}
+
+}
