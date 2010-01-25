a212e2bed2a57c5fef02da95a528db9e16db9f46
kzn <kzn@iitp.ru>
2010-01-25 18:29:02 +0300

    Added documentation dataset Interface

--- src/ru/iitp/proling/svm/Dataset.java
+++ src/ru/iitp/proling/svm/Dataset.java
@@ -1,23 +1,62 @@
 package ru.iitp.proling.svm;
 
 import ru.iitp.proling.common.Alphabet;
-
+/**
+ * Dataset interface. Dataset is a set of samples, represented by vector and it's labeling.
+ * Additionally, dataset provides a qid metadata - query id of the sample, if any. This is used
+ * in ranking applications
+ * @author ant
+ *
+ */
 public interface Dataset {
-	/// Return total number of samples in the dataset
+	/**
+	 * Get number of samples in the dataset
+	 * @return Number of samples in the dataset
+	 */
 	int size();
 	
-	/// Return greatest dimension found in the dataset
+	/**
+	 * Find dimension of dataset. Find the maximal used vector dimension of samples
+	 * @return Maximal dataset dimension
+	 */
 	int max_dim();
 	
-	/// Return sample idx
+	/**
+	 * Get sample from the dataset. Returns a reference, not a fresh object 
+	 * @param idx index of the sample
+	 * @return SparseVector representing the sample. 
+	 */
 	SparseVector vec(int idx);
 	
-	/// easy metadata access
+	/**
+	 * Return target index of label of sample \a idx, as mapped by internal alphabet
+	 * @param idx sample index
+	 * @return index of the target
+	 */
 	int target(int idx);
+	/**
+	 * Return query id of sample \a idx
+	 * @param idx sample index
+	 * @return query id, 0 if none
+	 */
 	int qid(int idx);
 	
+	/**
+	 * Return array of targets of all samples.
+	 * @return array of size size() of targets of all samples
+	 */
 	int[] targets();
+	
+	/**
+	 * Return array of query ids of all samples
+	 * @return array of size size() of query ids of all samples
+	 */
 	int[] qids();
+	
+	/**
+	 * Get the alphabet used.
+	 * @return Alphabet, used to map input label to int
+	 */
 	Alphabet alphabet();
 
 
