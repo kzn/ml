2405bd04c8ef120705913bbf9bf34a66c29e7598
kzn <kzn@iitp.ru>
2010-01-25 18:57:46 +0300

    Added documentation for classifier interfaces

--- src/ru/iitp/proling/svm/BinaryClassifier.java
+++ src/ru/iitp/proling/svm/BinaryClassifier.java
@@ -2,6 +2,12 @@ package ru.iitp.proling.svm;
 
 import ru.iitp.proling.svm.kernel.Kernel;
 
+/**
+ * Binary classification common class. Basic interface to binary predictor that has 2 outcomes.
+ * This a indirect interface, as any binary classfier expected to conform to defined functions
+ * @author ant
+ *
+ */
 public class BinaryClassifier{
 	protected double[] w;
 	protected Kernel kernel;
@@ -16,10 +22,20 @@ public class BinaryClassifier{
 	}
 
 
+	/**
+	 * Classify vector
+	 * @param v vector
+	 * @return class of vector, -1 if negative in case of indicator classification
+	 */
 	int classify(SparseVector v) {
 		return kernel.dot(w, v) > 0.0? positive : negative;
 	}
 	
+	/**
+	 * Get score of vector v, by this classifier. Can be used for various generalization performance measures.
+	 * @param v vector
+	 * @return score 
+	 */
 	double score(SparseVector v){
 		return kernel.dot(w, v);
 	}
--- src/ru/iitp/proling/svm/OVRClassifier.java
+++ src/ru/iitp/proling/svm/OVRClassifier.java
@@ -3,14 +3,15 @@ package ru.iitp.proling.svm;
 import ru.iitp.proling.svm.kernel.Kernel;
 
 /**
- * One vs. Rest classifier.
+ * One vs. Rest classifier. Used as a detector - i.e. on positive classification returns class. 
+ * On negative classification returns -1(default value to negative evidence)
  * @author ant
  *
  */
 
 public class OVRClassifier extends BinaryClassifier {
 	public OVRClassifier(double[] w, Kernel kernel, int positive){
-		super(w, kernel, positive, 0);
+		super(w, kernel, positive, -1);
 	}
 	
 }
