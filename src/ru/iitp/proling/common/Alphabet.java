d51e12de04c2507c1b1d5d554d8634ecf1c91206
kzn <kzn@iitp.ru>
2010-01-25 18:51:25 +0300

    Added documention to the Alphabet class

--- src/ru/iitp/proling/common/Alphabet.java
+++ src/ru/iitp/proling/common/Alphabet.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 /**
- * Fast mapper between Object <-> int
+ * Fast mapper between Object <-> int.  
  * @author ant
  *
  */
@@ -13,17 +13,32 @@ public class Alphabet<T> implements Serializable{
 	protected TObjectIntHashMap<T> map;
 	protected ArrayList<T> entries;
 	
+	/**
+	 * Create empty Alphabet object 
+	 * @param capacity initial capacity of Alphabet object
+	 * @param emptyObject default(empty) object in the Alphabet
+	 */
 	public Alphabet(int capacity, T emptyObject){
 		map = new TObjectIntHashMap<T>();
 		entries =  new ArrayList<T>();	
 		entries.add(emptyObject);
 		map.put(emptyObject, 0);
 	}
-	
+	/**
+	 * Get object at index
+	 * @param i index in the alphabet
+	 * @return object at index in the alphabet
+	 */
 	public T get(int i){
 		return entries.get(i);
 	}
 	
+	/**
+	 * Get/add object to alphabet
+	 * @param object object to lookup
+	 * @param add if true adds object to alphabet if missing
+	 * @return index of object, or -1 if not found
+	 */
 	public int get(T object, boolean add){
 		if(object == null)
 			throw new IllegalArgumentException ("Can't lookup \"null\" in an Alphabet.");
@@ -41,14 +56,26 @@ public class Alphabet<T> implements Serializable{
 		return retIndex;
 	}
 	
+	/**
+	 * Simple get. By default, adds missing object
+	 * @param object
+	 * @return index of object
+	 */
 	public int get(T object){
 		return get(object, true);
 	}
 	
+	/**
+	 * Get number of entries in the alphabet
+	 * @return number of entries in the alphabet
+	 */
 	public int size(){
 		return entries.size();
 	}
 	
+	/**
+	 * Convert alphabet object to String representation
+	 */
 	public String toString(){
 		StringBuilder sb = new StringBuilder();
 		sb.append("Alphabet:\n");
