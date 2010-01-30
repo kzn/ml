c2dfe80f574ac37f11b39441a68e3240ec502668
ant <devnull@localhost>
2010-01-31 01:46:19 +0300

    Now correct general ensemble scorer

--- src/ru/iitp/proling/svm/EnsembleScorer.java
+++ src/ru/iitp/proling/svm/EnsembleScorer.java
@@ -1,28 +1,38 @@
 package ru.iitp.proling.svm;
 
+import gnu.trove.TDoubleArrayList;
+
 import java.util.List;
 
 import ru.iitp.proling.svm.kernel.Kernel;
 
 public class EnsembleScorer extends Scorer {
 	
-	protected List<double[]> models;
-	protected double[] alpha;
+	protected List<Scorer> scorers;
+	protected TDoubleArrayList alpha;
 	protected Kernel kernel;
 	
-	public EnsembleScorer(List<double[]> models, double[] alpha, Kernel kernel){
-		assert(models.size() == alpha.length);
-		this.models = models;
+	public EnsembleScorer(List<Scorer> scorers, TDoubleArrayList alpha, Kernel kernel){
+		assert(scorers.size() == alpha.size());
+		this.scorers = scorers;
 		this.alpha = alpha;
 	}
 
 	@Override
 	public double score(SparseVector v) {
 		double res = 0;
-		for(int i = 0; i != models.size(); i++)
-			res += kernel.dot(models.get(i), v) * alpha[i];
-		
+		for(int i = 0; i != scorers.size(); i++)
+			res += scorers.get(i).score(v)* alpha.get(i);
 		return res;
+	}
+	
+	public void addScorer(Scorer scorer, double alpha){
+		this.alpha.add(alpha);
+		this.scorers.add(scorer);
+	}
+	
+	public int size(){
+		return alpha.size();
 	}
 
 }
