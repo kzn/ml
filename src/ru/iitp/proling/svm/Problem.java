e7e0863e4f835b03e2f9ae2ebf334a282f31fb25
ant <devnull@localhost>
2010-02-06 12:25:43 +0300

    Removing unused abstract classes

--- src/ru/iitp/proling/svm/Problem.java
+++
@@ -1,7 +0,0 @@
-package ru.iitp.proling.svm;
-
-import ru.iitp.proling.svm.kernel.Kernel;
-
-public abstract class Problem {
-	public abstract Classifier solve(Dataset dataset, Kernel kernel, Solver solver);
-}
