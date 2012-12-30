cbe4ad2ebd2a46696ad4ea5787934a7e86782efc
Anton Kazennikov <kazennikov@gmail.com>
2012-12-30 12:40:39 +0400

    adding javadocs

--- src/main/java/name/kazennikov/ml/dataset/AbstractSVMLightReader.java
+++ src/main/java/name/kazennikov/ml/dataset/AbstractSVMLightReader.java
@@ -9,6 +9,27 @@ import java.io.IOException;
 import java.io.Reader;
 import java.util.StringTokenizer;
 
+/**
+ * Reader for SVM-Light sample format.
+ * <p><p>
+ * The format is label + instance sparse vector: <p>
+ * [label] cost:[value] qid:[value] [index_1]:[value_1] [index_2]:[value_2] ... [index_n]:[value_n] \n
+ * <p><p>
+ * For example: 1 cost:5.0 qid:3 1:0.5 5:0.02 7:-4
+ * <p><p>
+ * This class should be extended with custom label parser. This is needed as the label
+ * could be a single real number (for binary classification 1.0/-1.0, or regression), integer number (for
+ * multiclass problem or ordinal regression), or set of numbers (for multicategory classification).
+ * 
+ * <p><p>
+ * The sparse vector is encoded by non-zero coordinate values. There are also special sample properties:<ul>
+ * <li> <b>cost</b> - the importance of this sample
+ * <li> <b>qid</b> - query id of the sample (needed for ordinal regression, where list or samples with same qid are 
+ * grouped in one logical training sample)
+ * 
+ * @author kzn
+ *
+ */
 public abstract class AbstractSVMLightReader implements Closeable {
 	BufferedReader br;
 	
@@ -28,6 +49,11 @@ public abstract class AbstractSVMLightReader implements Closeable {
 			br.close();
 	}
 	
+	/**
+	 * Read next sample.
+	 * @return true if sample is read successfully
+	 * @throws IOException
+	 */
 	public boolean readNext() throws IOException {
 		while(true) {
 			String line = br.readLine();
@@ -77,6 +103,10 @@ public abstract class AbstractSVMLightReader implements Closeable {
 		}
 	}
 
+	/**
+	 * Parse label token
+	 * @param nextToken
+	 */
 	protected abstract void parseLabel(String nextToken);
 	
 	public int[] indexes() {
